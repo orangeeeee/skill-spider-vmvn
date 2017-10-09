6e988555afbd26b9de2e618eecc81e8d78a56e0c
orangeeeee <orangeeeee.ore.ore@gmail.com>
2017-10-09 13:30:41 +0900

    second

---
+++ src/main/java/jp/co/skill/spider/SkillSpiderVmvnApplication.java
@@ -0,0 +1,12 @@
+package jp.co.skill.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSpiderVmvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSpiderVmvnApplication.class, args);
+	}
+}
